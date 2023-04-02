9230b7cad41b874f29fd113954bc4bf650882098
coderrick <derrick.duncan@temple.edu>
2023-04-02 02:56:34 -0400

    UI

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scratch Of(f(x))
+
+Scratch Off DApp for NFT Lottery tickets.
+
+Symbol: OFF
+
+## Architecture:
+
+Ocean Protocol-Provides marketplace for people to buy NFT Scratch off tickets minting new tickets with different attributes to be purchased.
+
+Owl Protocol-ERC721Dna, uses the standard to introduce new NFT stickers to Scratch off tickets.
+
---
+++ src/pages/About.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1 style={{ textShadow: "none" }}>About</h1>
+      <br />
+      <p style={{ margin: "auto", textShadow: "none" }}>Scratch Of(f(x)) is the place to buy digital scratch off 
+      tickets for instant lottery rewards.
+      </p>
+    </>
+
+  );
+};
+
+export default About;
